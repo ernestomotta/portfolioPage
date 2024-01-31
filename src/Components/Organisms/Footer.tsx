d029ae8f3a03eca430019f94118723f14041716d
ernestomotta <luisernestomotta@Luiss-MacBook-Air.local>
2024-01-30 23:57:56 -0600

    added footer and navbar skeleton

---
+++ src/Components/Organisms/Footer.tsx
@@ -0,0 +1,9 @@
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+
+export default function Footer() {
+  return (
+    <footer className="flex items-center justify-center m-12">
+      <WhatsAppIcon className="text-black waButton bg-white" />
+    </footer>
+  );
+}
